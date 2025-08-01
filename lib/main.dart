562a7ce44026bf90d510a106c27070dcc675be99
hyarts <hanyi12291@gmail.com>
2025-08-01 18:06:21 +0900

    개발환경 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro_timer/screen/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: '뽀모도로 타이머',
+      home: HomeScreen(),
     );
   }
 }
